5af711d984f7125d8594c9fbbcbc0490b7bd48ea
Richard Clark <rclark@c2i.fr>
2017-11-14 18:23:07 +0100

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  showUpdate = false;
+
+  constructor(private swUpdate: SwUpdate) { }
+  onUpdate() {
+    window.location.reload();
+  }
+
+  ngOnInit(): void {
+    this.swUpdate.available.subscribe(event => {
+
+      console.log('[App] Update available: current version is', event.current, 'available version is', event.available);
+      this.showUpdate = true;
+    });
+  }
 }
